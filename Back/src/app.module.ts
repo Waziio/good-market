322c310c5625eb65656edb09a882bab6793e02f6
Mohamed Azdoud <mazdoud-ext@cogelec.fr>
2023-08-18 13:41:05 +0200

    env file

--- Back/src/app.module.ts
+++ Back/src/app.module.ts
@@ -7,21 +7,24 @@ import { CategoryModule } from './category/category.module';
 import { Category } from './category/category.model';
 import { ProductModule } from './product/product.module';
 import { Product } from './product/product.model';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+export const config = new ConfigService();
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
     SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'perso',
+      host: config.get('DB_HOST'),
+      port: config.get('DB_PORT'),
+      username: config.get('DB_USER'),
+      password: config.get('DB_PASSWORD'),
+      database: config.get('DB_NAME'),
       models: [User, Category, Product],
       autoLoadModels: true,
       synchronize: true,
-      retryAttempts: 3
+      retryAttempts: 3,
     }),
     AuthModule,
     UsersModule,
