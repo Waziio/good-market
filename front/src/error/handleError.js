4d77b2accffda541a27e5acd0c3b1264f531d710
mohamed <mohamed.azd49@gmail.com>
2023-10-26 21:34:35 +0200

    fix warnings eslint

--- front/src/error/handleError.js
+++ front/src/error/handleError.js
@@ -3,7 +3,7 @@ export const handleLoginError = (err) => {
     const error = err.response.data;
     if (error.message === "Invalid password") {
       return "Mot de passe incorrect";
-    } else if (error.statusCode == 404 && error.message === "User") {
+    } else if (parseInt(error.statusCode) === 404 && error.message === "User") {
       return "Adresse email incorrecte";
     }
   }
